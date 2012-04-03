257a2a461c36522dbac1be538f1e9fe0148c3c48
Matt Baxter <matt@phozop.net>
2012-04-03 01:58:26 -0400

    Add command def

--- src/to/joe/j2mc/seniorfun/J2MC_SeniorFun.java
+++ src/to/joe/j2mc/seniorfun/J2MC_SeniorFun.java
@@ -6,11 +6,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import to.joe.j2mc.seniorfun.command.FireAllAdminsCommand;
-import to.joe.j2mc.seniorfun.command.KickAllCommand;
-import to.joe.j2mc.seniorfun.command.MadagascarCommand;
-import to.joe.j2mc.seniorfun.command.MaintenanceCommand;
-import to.joe.j2mc.seniorfun.command.SayCommand;
+import to.joe.j2mc.seniorfun.command.*;
 
 public class J2MC_SeniorFun extends JavaPlugin implements Listener {
 
@@ -34,6 +30,7 @@ public class J2MC_SeniorFun extends JavaPlugin implements Listener {
         this.getCommand("say").setExecutor(new SayCommand(this));
         this.getCommand("maintenance").setExecutor(new MaintenanceCommand(this));
         this.getCommand("firealladmins").setExecutor(new FireAllAdminsCommand(this));
+        this.getCommand("setspawn").setExecutor(new SetSpawnCommand(this));
 
         this.saveConfig();
 
